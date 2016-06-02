9791e0d8f0c67b3da1883dd08bc13d2a15dff250
赵瑞鹏 <rpdict@gmail.com>
2016-06-02 15:42:28 +0800

    change blogsView

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,23 +3,60 @@
 
 @section('content')
   {{--<title>{{ config('blog.title') }}</title>--}}
+  <style>
+      body {
+          background-color: #f5f8fa;
+      }
+
+      .jumbotron {
+          background-color: #fff;
+          margin-bottom: 0px;
+          border-bottom: solid 1px #f3f3f3;
+          padding-top: 28px;
+          padding-bottom: 28px;
+          border-radius: 0px;
+      }
+
+      .container .jumbotron {
+          border-radius: 0px;
+      }
+
+      .jumbotron hr{
+          width: 100%;
+      }
+
+      .jumbotron p{
+          font-size: 14px;
+      }
+      .container ul{
+          padding-left: 0px;
+      }
+
+      .jumbotron li{
+          list-style-type: none;
+      }
+  </style>
 
 <div class="container">
     <h1>{{ config('blog.title') }}</h1>
     <h5>Page {{ $posts->currentPage() }} of {{ $posts->lastPage() }}</h5>
-    <hr>
+    {{--<hr>--}}
     <ul>
+
         @foreach ($posts as $post)
+            <div class="jumbotron">
             <li>
-                <a href="/blog/show/{{ $post->id }}">{{ $post->title }}</a>
+                <h4><a href="/blog/show/{{ $post->id }}">{{ $post->title }}</a></h4>
                 <em>({{ $post->created_at->format('M jS Y g:ia') }})</em>
                 <p>
                     {{ str_limit($post->content) }}
                 </p>
             </li>
+            </div>
         @endforeach
+
     </ul>
-    <hr>
+    {{--<hr>--}}
     {!! $posts->render() !!}
 </div>
 
