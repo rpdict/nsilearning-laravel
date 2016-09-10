6a1f8c83f09eba7efd1da7e4212a148a23352a0a
rpdict <rpdict@gmail.com>
2016-09-10 17:34:25 +0800

    Change database config

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
 //            'database'  => env('DB_DATABASE', 'forge'),
-            'database'  => 'laravel-nsilearning',
+            'database'  => 'nsilearning',
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
