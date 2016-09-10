b65cf9de524fb01862c664a2b9866d1d9c9344a3
rpdict <rpdict@gmail.com>
2016-09-10 18:02:53 +0800

    merge .env.example

--- config/database.php
+++ config/database.php
@@ -55,8 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-//            'database'  => env('DB_DATABASE', 'forge'),
-            'database'  => 'nsilearning',
+            'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
